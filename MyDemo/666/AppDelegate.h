f858079776c7dc146f092d54224451887319b27c
zhwb <zhuwb@hori-gz.com>
2018-01-23 14:21:02 +0800

    我要提交项目

---
+++ MyDemo/666/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  666
+//
+//  Created by owen on 2018/1/23.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/666/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  666
+//
+//  Created by owen on 2018/1/23.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
